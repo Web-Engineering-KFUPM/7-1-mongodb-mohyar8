f846f108308ca152bf281a79774b4587b19a8d78
mohyar8 <mohyar44@gmail.com>
2025-11-17 23:51:47 +0300

    submission

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -170,22 +170,46 @@
 import mongoose from "mongoose";
 
 // establish connection
-
+mongoose.connect("mongodb+srv://alobaidibrahim0_db_user:dcG7OGpZtBaL15IK@cluster0.ueqkapg.mongodb.net/")
+.then(() => console.log("Connected"))
+.catch(err => console.log(err));
 
 // define schema
+const studentSchema = new mongoose.Schema({
+   name: String,
+   age: Number,
+   major: String
+});
+const Student = mongoose.model("Student", studentSchema);
 
 
 // create document
-
+async function createStudents() {
+   await Student.insertMany([
+      { name: "Ali", age: 21, major: "CS" },
+      { name: "Sara", age: 23, major: "SE" }
+   ]);
+   console.log("✅ Inserted");
+}
+createStudents();
 
 // read document
-
+async function readStudents() {
+   const all = await Student.find();
+   console.log(all);
+}
+readStudents();
 
 // update document
-
+async function updateStudent() {
+         await Student.updateOne({ name: "Ali" }, { age: 22 });
+         console.log("✅ Updated Ali");
+}
+updateStudent();
 
 // delete document
-
-
-
-
+async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+}
+deleteStudent();
